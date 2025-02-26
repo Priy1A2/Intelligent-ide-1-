f6173db81edd493aecd7344616aeb67d97224a54
Priy1A2 <156531931+Priy1A2@users.noreply.github.com>
2025-02-26 11:59:58 +0530

    Delete server directory

--- server/test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBvsCWfAhgNSPW8rQEu6187lwDbx7UGz8o")
-
-model = genai.GenerativeModel("gemini-pro")
-response = model.generate_content("Hello, Gemini! tell me your achivements")
-print(response.text)
